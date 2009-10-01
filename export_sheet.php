d3ef48d20bc0f6a8290da0117af771d8f16aa67f
wjames5 <will@tekimaki.com>
2009-10-01 13:45:48 +0000

    fix LGPL license ref from non-existant license.txt file to lgpl url

--- export_sheet.php
+++ export_sheet.php
@@ -1,11 +1,11 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_sheets/export_sheet.php,v 1.2 2008/06/25 22:21:23 spiderr Exp $
+// $Header: /cvsroot/bitweaver/_bit_sheets/export_sheet.php,v 1.3 2009/10/01 13:45:48 wjames5 Exp $
 
 // Based on galleries.php
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 // Initialization
 require_once ('../bit_setup_inc.php');
--- sheet/excel/writer/index.php
+++ sheet/excel/writer/index.php
@@ -1,10 +1,10 @@
 <?php
 
-// $Header: /cvsroot/bitweaver/_bit_sheets/sheet/excel/writer/index.php,v 1.1 2005/08/07 11:35:11 wolff_borg Exp $
+// $Header: /cvsroot/bitweaver/_bit_sheets/sheet/excel/writer/index.php,v 1.2 2009/10/01 13:45:48 wjames5 Exp $
 
 // Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
-// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
 
 // This redirects to the sites root to prevent directory browsing
 
